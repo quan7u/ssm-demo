baee4c7feda645e6fa3c736a15531e2b71b88240
William <87509674+WilliamSESE@users.noreply.github.com>
2021-08-19 15:03:09 +0800

    Update AdminServiceImpl.java
    添加 @Service 注解

--- src/main/java/service/impl/AdminServiceImpl.java
+++ src/main/java/service/impl/AdminServiceImpl.java
@@ -2,6 +2,7 @@ package service.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,6 +10,7 @@ import mapper.AdminMapperCustom;
 import po.Admin;
 import service.AdminService;
 
+@Service
 @Transactional
 public class AdminServiceImpl implements AdminService{
 
