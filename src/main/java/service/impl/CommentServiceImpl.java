08253973f4fb9ad8732d14e283de3f7bdd6f109f
William <87509674+WilliamSESE@users.noreply.github.com>
2021-08-19 14:59:59 +0800

    Update CommentServiceImpl.java
    添加 @Service 注解

--- src/main/java/service/impl/CommentServiceImpl.java
+++ src/main/java/service/impl/CommentServiceImpl.java
@@ -2,6 +2,7 @@ package service.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,6 +13,7 @@ import po.Page;
 import service.CommentService;
 import utils.DateConvert;
 
+@Service
 @Transactional
 public class CommentServiceImpl implements CommentService {
 
